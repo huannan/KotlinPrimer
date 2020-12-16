c7154e932196fc8211799565b4ec1b59e66f10d5
吴焕楠 <wuhuannan@meizu.com>
2020-12-16 18:25:01 +0800

    补充延迟初始化

--- app/src/main/java/com/nan/kotlinprimer/day1_basic/Demo.kt
+++ app/src/main/java/com/nan/kotlinprimer/day1_basic/Demo.kt
@@ -4,6 +4,7 @@ package com.nan.kotlinprimer.day1_basic
 var age1: Int = 18
 
 // val 表示一个不可变的变量，相当于加了final修饰
+// val 是 Kotlin 在 Java 的「变量」类型之外，又增加的一种变量类型：只读变量。它只能赋值一次，不能修改。而 var 是一种可读可写变量。
 val age2: Int = 18
 
 // 类型是编译器可以推断出来的就不用写类型
@@ -15,12 +16,19 @@ var name1: String = "小楠总"
 // 可空变量
 var name2: String? = null
 
+// 延迟初始化
+// 这个 lateinit 的意思是：告诉编译器我没法第一时间就初始化，但我肯定会在使用它之前完成初始化的。
+// 它的作用就是让 IDE 不要对这个变量检查初始化和报错。换句话说，加了这个 lateinit 关键字，这个变量的初始化就全靠你自己了，编译器不帮你检查了。
+lateinit var name3: String
+
 fun main() {
     testNullSafe()
 
     val res = testFunction(name1)
 
     testTemplate()
+
+    name3 = ""
 }
 
 /**
--- app/src/main/java/com/nan/kotlinprimer/day2_java_call/demo4/Demo.kt
+++ app/src/main/java/com/nan/kotlinprimer/day2_java_call/demo4/Demo.kt
@@ -1,12 +1,22 @@
 package com.nan.kotlinprimer.day2_java_call.demo4
 
 /**
- * Kotlin 没有装箱类
+ * Kotlin 在语言层面简化了 Java 中的 int 和 Integer
+ *
+ * 但是我们对是否装箱的场景还是要有一个概念，因为这个牵涉到程序运行时的性能开销。因此在日常的使用中，对于 Int 这样的基本类型，尽量用不可空变量。
+ * 简单来说，原先在 Java 里的基本类型，类比到 Kotlin 里面，条件满足如下之一就不装箱：不可空类型、使用 IntArray、FloatArray 等
  */
 fun main() {
     // 你会发现只调用了 int 参数的方法，通过这个示例可以看出来 Kotlin 是没有装箱类的概念的
     val printInteger = IntegerPrinter()
     printInteger.printNum(10)
+
+    // 可空类型，实质上是装箱类
+    var num: Int? = 10
+    printInteger.printNum(num)
+
+    // 这里也不是装箱
+    var array: IntArray = intArrayOf(1, 2)
 }
 
 /**
